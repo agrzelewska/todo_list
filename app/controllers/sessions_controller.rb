f9e0b3c479422af9e31bec1dd168e982f6b2a869
agrzelewska <agatagrzelewska@gmail.com>
2018-07-17 23:29:13 +0200

    Create session action, views for logged in and logged out users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(mail: params[:session][:mail].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
