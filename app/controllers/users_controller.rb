c8e1d6b34061fd3fd65ae4c17dd4a6aec802a41a
agrzelewska <agatagrzelewska@gmail.com>
2018-07-18 22:56:05 +0200

    Adding Logout action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to your To Do List!"
       redirect_to @user
     else
